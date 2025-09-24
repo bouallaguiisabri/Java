a5c9ce287a19b30726be2a512c1ad7c9dee09453
bouallagui sabri <sabribouallagui@gmail.com>
2025-09-24 23:42:26 +0100

    java prosit2

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,128 @@
 import java.util.Scanner;
 
-class ZooManagement {
-    int nbrCages;
-    String zooName;
+class Animal {
+    private String name;
+    private int age;
+    private String family;
+    private boolean isMammal;
+
+
+    public Animal(String name, int age, String family, boolean isMammal) {
+        this.name = name;
+        this.age = age;
+        this.family = family;
+        this.isMammal = isMammal;
+    }
+
+    // Redéfinition de toString()
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", family='" + family + '\'' +
+                ", isMammal=" + isMammal +
+                '}';
+    }
 }
 
-class Main {
+class Zoo {
+    private Animal[] animals;
+    private String name;
+    private String city;
+    private int nbrCages;
+    private int animalCount = 0;
+
+
+    public Zoo(String name, String city, int nbrCages) {
+        this.name = name;
+        this.city = city;
+        this.nbrCages = nbrCages;
+        this.animals = new Animal[25];
+    }
+
+
+    public void addAnimal(Animal a) {
+        if (animalCount < animals.length) {
+            animals[animalCount] = a;
+            animalCount++;
+            System.out.println("✅ Animal ajouté avec succès !");
+        } else {
+            System.out.println("❌ Zoo plein, impossible d'ajouter plus d'animaux !");
+        }
+    }
+
+    public void displayZoo() {
+        System.out.println("Zoo name: " + this.name);
+        System.out.println("City: " + this.city);
+        System.out.println("Number of cages: " + this.nbrCages);
+        System.out.println("Animaux dans le zoo:");
+        for (int i = 0; i < animalCount; i++) {
+            System.out.println("  - " + animals[i]);
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "Zoo{" +
+                "name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                ", nbrCages=" + nbrCages +
+                ", animaux=" + animalCount +
+                '}';
+    }
+}
+
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ZooManagement z1 = new ZooManagement();
 
 
-        String nom;
-        do {
-            System.out.print("Veuillez saisir le nom du zoo : ");
-            nom = sc.nextLine().trim();
-            if (nom.isEmpty()) {
-                System.out.println("Le nom du zoo ne doit pas être vide.");
-            }
-        } while (nom.isEmpty());
-        z1.zooName = nom;
+        System.out.print("Entrez le nom du zoo: ");
+        String zooName = sc.nextLine();
+
+        System.out.print("Entrez la ville du zoo: ");
+        String city = sc.nextLine();
+
+        System.out.print("Entrez le nombre de cages: ");
+        int cages = sc.nextInt();
+        sc.nextLine();
+
+        Zoo myZoo = new Zoo(zooName, city, cages);
+
+        System.out.print("Combien d'animaux voulez-vous ajouter ? ");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("\n=== Animal " + (i + 1) + " ===");
+            System.out.print("Nom : ");
+            String name = sc.nextLine();
+
+            System.out.print("Âge : ");
+            int age = sc.nextInt();
+            sc.nextLine();
+
+            System.out.print("Famille : ");
+            String family = sc.nextLine();
+
+            System.out.print("Est-ce un mammifère ? (true/false) : ");
+            boolean isMammal = sc.nextBoolean();
+            sc.nextLine();
 
 
-        int cages;
-        do {
-            System.out.print("Veuillez saisir le nombre de cages : ");
-            while (!sc.hasNextInt()) {
-                System.out.println("Veuillez entrer un nombre valide.");
-                sc.next();
-            }
-            cages = sc.nextInt();
-            if (cages <= 0) {
-                System.out.println("Le nombre de cages doit être positif.");
-            }
-        } while (cages <= 0);
-        z1.nbrCages = cages;
+            Animal a = new Animal(name, age, family, isMammal);
+            myZoo.addAnimal(a);
+        }
 
 
-        System.out.println(z1.zooName + " comporte " + z1.nbrCages + " cages.");
+        System.out.println("\n=== Informations sur le Zoo ===");
+        myZoo.displayZoo();
+
+        System.out.println("\n=== Affichage avec toString() ===");
+        System.out.println(myZoo);
+
+        sc.close();
     }
 }
